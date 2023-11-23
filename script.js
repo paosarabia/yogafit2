bea9a3b9aa25e036ad7dbcd219e7de3d7f9a53f8
paosarabiam <mariapsarabia@gmail.com>
2023-11-23 11:09:54 -0300

    cambios

--- script.js
+++ script.js
@@ -77,6 +77,31 @@ document.addEventListener('DOMContentLoaded', function() {
     cancelButton.addEventListener('click', resetTimer);
   });
   
+  /* Carousel */
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const prevButton = document.querySelector('.prev-button');
+    const nextButton = document.querySelector('.next-button');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+  
+    let currentIndex = 0;
+  
+    function showCurrentSlide() {
+      carouselItems.forEach((item, index) => {
+        item.style.display = index === currentIndex ? 'block' : 'none';
+      });
+    }
+  
+    prevButton.addEventListener('click', function () {
+      currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+      showCurrentSlide();
+    });
+  
+    nextButton.addEventListener('click', function () {
+      currentIndex = (currentIndex + 1) % carouselItems.length;
+      showCurrentSlide();
+    });
+  });
 
 
   
